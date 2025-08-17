99b520e07feb7325bc764db22a78a65954953da1
Kenshu Hori <kenshuhori@gmail.com>
2025-08-17 15:15:26 +0900

    npx ts-node index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+import Anthropic from '@anthropic-ai/sdk';
+
+const client = new Anthropic({
+  apiKey: process.env['ANTHROPIC_API_KEY'], // This is the default and can be omitted
+});
+
+const message = await client.messages.create({
+  max_tokens: 1024,
+  messages: [{ role: 'user', content: 'Hello, Claude' }],
+  model: 'claude-sonnet-4-20250514',
+});
+
+console.log(message.content);
